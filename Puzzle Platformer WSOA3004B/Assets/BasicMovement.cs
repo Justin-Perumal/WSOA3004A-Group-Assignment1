ebba45e4474876b48026df765f1afc2ace584a89
Justin-Perumal <1452891@students.wits.ac.za>
2021-08-13 00:46:40 +0200

    Project file uploaded
    Initial commit of Project file
    
    check readme

---
+++ Puzzle Platformer WSOA3004B/Assets/BasicMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMovement : MonoBehaviour
+{
+    public float MoveSpeed; // Movespeed of the player
+    public float JumpForce; // How high player can jump
+    private float MoveInput; //Getting movement direction
+    private bool IsGrounded; //For later when character controller is completely fleshed out
+    public Rigidbody2D PlayerBody;
+
+    void Start()
+    {
+        PlayerBody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        MoveInput = Input.GetAxis("Horizontal");
+        PlayerBody.velocity = new Vector2(MoveInput*MoveSpeed, PlayerBody.velocity.y);
+
+        if(Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+
+    //Basic Jump function 
+    public void Jump()
+    {
+        PlayerBody.velocity = Vector2.up * JumpForce;
+    }
+}
