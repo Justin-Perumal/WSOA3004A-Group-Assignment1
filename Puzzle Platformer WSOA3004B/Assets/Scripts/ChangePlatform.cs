302404477672518be9239f87c05ad978d62828f3
Justin-Perumal <1452891@students.wits.ac.za>
2021-08-13 21:03:53 +0200

    started work on level
    Color changing platforms work properly and  combinations work. Have to work on sequencing

---
+++ Puzzle Platformer WSOA3004B/Assets/Scripts/ChangePlatform.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangePlatform : MonoBehaviour
+{
+    public ColourSwitch CS;
+    public string CurrentColour;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CS = GameObject.Find("Player").GetComponent<ColourSwitch>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D Collision)
+    {
+        if(Collision.CompareTag("White"))
+        {
+            gameObject.GetComponent<SpriteRenderer>().color = new Color(255,255,255);
+            CurrentColour = CS.Colour[CS.ColourCounter];
+        }
+        if(Collision.CompareTag("Red"))
+        {
+            gameObject.GetComponent<SpriteRenderer>().color = new Color(255,0,0);
+            CurrentColour = CS.Colour[CS.ColourCounter];
+        }
+        if(Collision.CompareTag("Green"))
+        {
+            gameObject.GetComponent<SpriteRenderer>().color = new Color(0,255,0);
+            CurrentColour = CS.Colour[CS.ColourCounter];
+        }
+        if(Collision.CompareTag("Blue"))
+        {
+            gameObject.GetComponent<SpriteRenderer>().color = new Color(0,0,255);
+            CurrentColour = CS.Colour[CS.ColourCounter];
+        }
+    }
+}
