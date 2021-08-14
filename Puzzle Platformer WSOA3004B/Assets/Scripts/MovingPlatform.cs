68496f0c0c099ffbf26c30b366044bf67864a9a7
SiyathandanaNontolwana <1462572@students.wits.ac.za>
2021-08-14 03:59:15 +0200

    Moving platforms working
    Added a scripts that uses waypoints to move platforms on player contact. Started work on level.

---
+++ Puzzle Platformer WSOA3004B/Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    [SerializeField] private GameObject[] Waypoints;
+    private int currentIndex;
+
+    [SerializeField] private float platformSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Keep track of current idext
+        if(Vector2.Distance(Waypoints[currentIndex].transform.position, transform.position) < 0.1f)
+        {
+            currentIndex++;
+            if(currentIndex >= Waypoints.Length)
+            {
+                currentIndex = 0;
+            }
+        }      
+    }
+
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        //Move platform towards target waypoint
+        if (collision.gameObject.CompareTag("Player") && Vector2.Distance(Waypoints[currentIndex].transform.position, transform.position) < 0.1f)
+        {
+            currentIndex++;
+            if (currentIndex >= Waypoints.Length)
+            {
+                currentIndex = 0;
+            }
+        }
+        transform.position = Vector2.MoveTowards(transform.position, Waypoints[currentIndex].transform.position, platformSpeed * Time.deltaTime);
+    }
+}
