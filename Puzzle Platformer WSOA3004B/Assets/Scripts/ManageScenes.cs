a5dd68ffc72586d663398081ea452b6e0db1fcd5
Justin-Perumal <1452891@students.wits.ac.za>
2021-08-16 00:17:25 +0200

    SceneManagement and Test Area
    Created the scene management for all scenes and the area for testing for my level

---
+++ Puzzle Platformer WSOA3004B/Assets/Scripts/ManageScenes.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ManageScenes : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadJustinLevel()
+    {
+        SceneManager.LoadScene("Justin_Level");
+    }
+
+    public void LoadSiyaLevel()
+    {
+        SceneManager.LoadScene("Siya_Level");
+    }
+
+    public void LoadWesLevel()
+    {
+        SceneManager.LoadScene("Wes_Level");
+    }
+
+    public void OnTriggerEnter2D(Collider2D Collider)
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
