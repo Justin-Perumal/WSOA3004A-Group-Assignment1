032b3bdd648b27e5325edaeb5222680895549a7b
Wesley_Huang_2244062 <2244062@students.wits.ac.za>
2021-08-22 21:03:29 +0200

    background color

---
+++ Puzzle Platformer WSOA3004B/Assets/Scripts/ColorLerp.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorLerp : MonoBehaviour
+{
+    public Color ColorOne;
+    public Color ColorTwo;
+    public float speed = 0.1f;
+    Color Current_color;
+    void Start()
+    {
+        Current_color = ColorOne;
+        StartCoroutine(colorswitch());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Current_color == ColorOne)
+        {
+            Current_color = ColorTwo;
+            Debug.Log("color = 2");
+        }
+        else
+        {
+            Current_color = ColorOne;
+            Debug.Log("color = 1");
+        }
+    }
+    IEnumerator colorswitch()
+    {
+
+
+
+        return null;
+    }
+}
---
+++ Puzzle Platformer WSOA3004B/Assets/Scripts/Fade.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fade : MonoBehaviour
+{
+    Color Color_Start;
+    Color Color_End;
+    public float duration = 3f;
+    Color lerpedColor = Color.white;
+
+    float t = 0;
+    bool Check;
+
+    public Color[] Color_array;
+    
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        lerpedColor = Color.Lerp(Color_Start, Color_End, t);
+        gameObject.GetComponent<Camera>().backgroundColor= lerpedColor;
+
+        if (Check == true)
+        {
+            t -= Time.deltaTime / duration;
+            if (t < 0.01f)
+                Check = false;
+        }
+        else
+        {
+            t += Time.deltaTime / duration;
+            if (t > 0.99f)
+                Check = true;
+            Color_Changer_Random();
+        }
+    }
+
+    void Color_Changer_Random()
+    {
+        int Random_num = Random.Range(0,Color_array.Length);
+        Color_Start = Color_End;
+        Color_End = Color_array[Random_num];
+    }
+    
+
+}
