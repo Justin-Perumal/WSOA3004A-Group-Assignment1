33af5ece1d89643b4e3d0d701491e88d9db86360
Wesley_Huang_2244062 <2244062@students.wits.ac.za>
2021-08-22 21:55:18 +0200

    BlackOut Background

--- Puzzle Platformer WSOA3004B/Assets/Scripts/Fade.cs
+++ Puzzle Platformer WSOA3004B/Assets/Scripts/Fade.cs
@@ -4,21 +4,16 @@ using UnityEngine;
 
 public class Fade : MonoBehaviour
 {
-    Color Color_Start;
-    Color Color_End;
+    public Color Color_Start;
+    public Color Color_End;
     public float duration = 3f;
     Color lerpedColor = Color.white;
 
     float t = 0;
     bool Check;
+    bool once;
 
-    public Color[] Color_array;
     
-    // Use this for initialization
-    void Start()
-    {
-        
-    }
 
     void FixedUpdate()
     {
@@ -30,22 +25,14 @@ public class Fade : MonoBehaviour
             t -= Time.deltaTime / duration;
             if (t < 0.01f)
                 Check = false;
+
         }
         else
         {
             t += Time.deltaTime / duration;
             if (t > 0.99f)
                 Check = true;
-            Color_Changer_Random();
         }
-    }
-
-    void Color_Changer_Random()
-    {
-        int Random_num = Random.Range(0,Color_array.Length);
-        Color_Start = Color_End;
-        Color_End = Color_array[Random_num];
-    }
-    
+    }    
 
 }
